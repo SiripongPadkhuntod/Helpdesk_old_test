5c5b9b50922a06c0337b513f3237be61ea65b438
Thyme <siripong.p64@rsu.ac.th>
2024-05-20 13:23:25 +0700

    delete auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import AuthPage from './Pages/Auth';
 import Dashboard from './Pages/Dashboard';
 import NewTicket from './components/ticket/CreateNewTicket';
 import Ticket from './components/ticket/Tickets';
@@ -11,7 +10,6 @@ const App = () => {
     <Router>
       <div>
         <Routes>
-          <Route path="/auth" element={<AuthPage />} /> {/* เพิ่ม Route ของ AuthPage */}
           <Route path="/" element={<Dashboard />} />
           <Route path="/tickets/new" element={<NewTicket />} />
           <Route path="/tickets" element={<Ticket />} />
